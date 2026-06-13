44ff3d536e39849a9d3263e1c76a255118164a7d
Maazifx <123500181+Maazifx@users.noreply.github.com>
2026-06-13 07:05:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Bot is working")
+import requests
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+message = """
+⚽ Football Bot Online
+
+GitHub Actions is working.
+Telegram connection successful.
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": "@wcupdates2026",
+        "text": message
+    }
+)
+
+print("Message sent")
