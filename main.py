86c61efb97b2024f150389ffaad942e9f40d1c64
Maazifx <123500181+Maazifx@users.noreply.github.com>
2026-06-13 07:24:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import requests
 import os
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+API_KEY = os.environ["API_KEY"]
 
-message = """
-⚽ Football Bot Online
+headers = {
+    "x-apisports-key": API_KEY
+}
 
-GitHub Actions is working.
-Telegram connection successful.
-"""
+response = requests.get(
+    "https://v3.football.api-sports.io/fixtures?live=all",
+    headers=headers
+)
+
+data = response.json()
+
+message = "⚽ LIVE MATCHES\n\n"
+
+if len(data["response"]) == 0:
+    message += "No live matches currently."
+else:
+    for match in data["response"][:5]:
+        home = match["teams"]["home"]["name"]
+        away = match["teams"]["away"]["name"]
+
+        home_score = match["goals"]["home"]
+        away_score = match["goals"]["away"]
+
+        minute = match["fixture"]["status"]["elapsed"]
+
+        message += (
+            f"{minute}'\n"
+            f"{home} {home_score}-{away_score} {away}\n\n"
+        )
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -20,4 +44,4 @@ requests.post(
     }
 )
 
-print("Message sent")
+print("Live scores posted")
